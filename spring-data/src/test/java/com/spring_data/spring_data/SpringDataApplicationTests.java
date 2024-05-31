2c442ac799dc0342d2ed7e8361ac51d958d4b5f0
AIDA <meandaida@gmail.com>
2024-05-30 19:18:48 -0500

    first code push

---
+++ spring-data/src/test/java/com/spring_data/spring_data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_data.spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
